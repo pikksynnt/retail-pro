3385091fd04d07d967b03cb0a1e04cdf7dbf46ed
Opick <opick@example.com>
2026-05-08 19:11:03 +0700

    Setting Sakti buat Vercel

--- api/index.php
+++ api/index.php
@@ -5,9 +5,17 @@ $cachePath = '/tmp/bootstrap/cache';
 if (!is_dir($cachePath)) {
     mkdir($cachePath, 0755, true);
 }
+
+// Tambah folder storage/framework/views di /tmp
+$viewPath = '/tmp/storage/framework/views';
+if (!is_dir($viewPath)) {
+    mkdir($viewPath, 0755, true);
+}
+
 putenv("APP_CONFIG_CACHE={$cachePath}/config.php");
 putenv("APP_ROUTES_CACHE={$cachePath}/routes.php");
 putenv("APP_SERVICES_CACHE={$cachePath}/services.php");
 putenv("APP_PACKAGES_CACHE={$cachePath}/packages.php");
+putenv("VIEW_COMPILED_PATH={$viewPath}");
 
 require __DIR__ . '/../public/index.php';
